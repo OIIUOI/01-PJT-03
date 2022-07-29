f63777575d5ee38fb80a29baf27a81608df18595
OIIUOI <eunjin4788@gmail.com>
2022-07-29 17:58:16 +0900

    알고리즘 모의고사

--- 1회차/이은진/3_직사각형길이찾기.py
+++ 1회차/이은진/3_직사각형길이찾기.py
@@ -1,3 +1,18 @@
 import sys
 
 sys.stdin = open("_직사각형길이찾기.txt")
+
+for t in range(1, int(input()) + 1):
+    rec_list = list(map(int, input().split()))
+    rec_dict = {}
+    for r in rec_list:
+        if r not in rec_dict:
+            rec_dict[r] = 1
+        elif r in rec_dict:
+            rec_dict[r] += 1
+
+    for e in rec_dict:
+        if rec_dict[e] == 1:
+            print(f'#{t} {e}')
+        elif rec_dict[e] == 3:
+            print(f'#{t} {e}')
--- 1회차/이은진/6_암호문1.py
+++ 1회차/이은진/6_암호문1.py
@@ -1,3 +1,20 @@
 import sys
 
 sys.stdin = open("_암호문1.txt")
+
+length = int(input())
+code = input().split()
+origin = {}
+
+# 암호문 origin 딕셔너리에 입력
+for i in range(length):
+    origin[i] = code[i]
+
+order_time = int(input())
+order = input().split('|')
+
+for ot in range(order_time):
+    for o in order:
+        # 스플릿아 3개로 쪼개줘 할 줄 모른다구
+        x,y,z = o.split('',3)
+        origin[x] = z
